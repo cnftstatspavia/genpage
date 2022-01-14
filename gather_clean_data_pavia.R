8141c158178573319e049dd21960acdc258befe6
cnftstats <95498151+cnftstats@users.noreply.github.com>
2022-01-14 10:13:57 +0100

    Add files via upload

--- gather_clean_data_pavia.R
+++ gather_clean_data_pavia.R
@@ -1,11 +1,17 @@
 # Setup --------------------------------------------------------------------------------------------
 library(data.table)
+library(lubridate)
 library(jsonlite)
 library(tidyr)
 library(httr)
 
+# Variables ----------------------------------------------------------------------------------------
 policy_id <- "4bf184e01e0f163296ab253edd60774e2d34367d0e7b6cbc689b567d"
+project <- "Pavia"
+time_now <- as_datetime(now())
 
+
+# Databases ----------------------------------------------------------------------------------------
 # Functions
 loj <- function (X = NULL, Y = NULL, onCol = NULL) {
   if (truelength(X) == 0 | truelength(Y) == 0) 
@@ -16,10 +22,9 @@ loj <- function (X = NULL, Y = NULL, onCol = NULL) {
 
 
 # Extract information from cnft.io -----------------------------------------------------------------
-api_link <- "https://api.cnft.io/market/listings"
-project <- "Pavia"
+api_link_cnft <- "https://api.cnft.io/market/listings"
 
-query <- function(page, url, project) {
+query <- function(page, url, project, sold) {
   httr::content(httr::POST(
     url = url, 
     body = list(
@@ -32,29 +37,15 @@ query <- function(page, url, project) {
       page = page, 
       verified = TRUE, 
       nsfw = FALSE, 
-      sold = FALSE, 
+      sold = sold, 
       smartContract = NULL
     ), 
     encode = "json"
   ), simplifyVector = TRUE)
 }
 
-query_all <- function(url, project) {
-  n <- query(1L, url, project)[["count"]]
-  out <- vector("list", n)
-  for (i in seq_len(n)) {
-    out[[i]] <- query(i, url, project)[["results"]]
-    if (length(out[[i]]) < 1L)
-      return(out[seq_len(i - 1L)])
-  }
-  out
-}
-
-print("ok 1")
-
-CNFT <- query_all(api_link, project) |> lapply(data.table) |> rbindlist(fill = TRUE)
-
-print("ok 2")
+CNFT <- query_n(api_link_cnft, project, sold = FALSE) |>
+  lapply(data.table) |> rbindlist(fill = TRUE)
 
 CNFT <- CNFT[asset.policyId == policy_id]
 CNFT[, asset        := asset.metadata.name]
@@ -73,6 +64,21 @@ CNFT[type == "listing", last_offer := NA]
 CNFT <- CNFT[, .(asset, type, price, last_offer, sc, market, link)]
 
 
+# CNFT sales ---------------------------------------------------------------------------------------
+CNFTS <- query_n(api_link_cnft, project, 11, sold = TRUE) |>
+  lapply(data.table) |> rbindlist(fill = TRUE)
+
+CNFTS[, asset         := asset.metadata.name]
+CNFTS[, price         := price/10**6]
+CNFTS[, market        := "cnft.io"]
+CNFTS[, sold_at       := as_datetime(soldAt)]
+CNFTS[, sold_at_hours := difftime(time_now, sold_at, units = "hours")]
+CNFTS[, sold_at_days  := difftime(time_now, sold_at, units = "days")]
+
+CNFTS <- CNFTS[order(-sold_at), .(asset, price, sold_at, sold_at_hours, sold_at_days, market)]
+CNFTS <- CNFTS[sold_at_hours <= 24*3]
+
+
 # Extract information from jpg.store ---------------------------------------------------------------
 # jpg.store/api/policy - all supported policies
 # jpg.store/api/policy/[id]/listings - listings for a given policy
@@ -88,16 +94,48 @@ JPG[, price        := price/10**6]
 JPG[, sc           := "yes"]
 JPG[, market       := "jpg.store"]
 
-print("ok 3")
-
 JPG <- JPG[, .(asset, type = "listing", price, last_offer = NA, sc, market, link)]
 
-# Merge info
+
+# JPG sales ----------------------------------------------------------------------------------------
+api_link <- sprintf("jpg.store/api/policy/%s/sales", policy_id)
+
+JPGS <- data.table(fromJSON(rawToChar(GET(api_link)$content)))
+JPGS[, price         := price_lovelace]
+JPGS[, asset         := asset_display_name]
+JPGS[, price         := price/10**6]
+JPGS[, market        := "jpg.store"]
+JPGS[, sold_at       := as_datetime(purchased_at)]
+JPGS[, sold_at_hours := difftime(time_now, sold_at, units = "hours")]
+JPGS[, sold_at_days  := difftime(time_now, sold_at, units = "days")]
+
+JPGS <- JPGS[order(-sold_at), .(asset, price, sold_at, sold_at_hours, sold_at_days, market)]
+JPGS <- JPGS[sold_at_hours <= 24*3]
+
+
+# Merge markets data -------------------------------------------------------------------------------
+# Listings
 DT <- rbindlist(list(CNFT, JPG), fill = TRUE, use.names = TRUE)
 
-print("ok 4")
+# Sales
+DTS <- rbindlist(list(CNFTS, JPGS), fill = TRUE, use.names = TRUE)
+
+# Add data collection timestamp
+DT[, data_date := time_now]
+DTS[, data_date := time_now]
+
 
 # Save ---------------------------------------------------------------------------------------------
 saveRDS(DT, file = "data/DT.rds")
+saveRDS(DTS, file = "data/DTS.rds")
 
-print("ok 5")
+
+# Database evolution -------------------------------------------------------------------------------
+DTE <- copy(DT)
+if (file.exists("data/DTE_pavia.rds")) {
+  cat("File data/DTE exists:", file.exists("data/DTE_pavia.rds"), "\n")
+  DTE_old <- readRDS("data/DTE_pavia.rds")
+  DTE <- rbindlist(list(DTE, DTE_old))
+  DTE <- DTE[difftime(time_now, data_date, units = "hours") <= 24] # Only retain last 24 hours
+}
+saveRDS(DTE, file = "data/DTE_pavia.rds")
